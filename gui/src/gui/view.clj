e7c5e514bed67e761a0ccdbb15171304bb33535d
Matthew Carter <m@ahungry.com>
2019-07-03 23:31:50 -0400

    need to handle tab closing

--- gui/src/gui/view.clj
+++ gui/src/gui/view.clj
@@ -8,6 +8,7 @@
    [javafx.scene.canvas Canvas]))
 
 (def *state (atom {:stub nil
+                   :ticket-tabs []
                    :ticket nil
                    :tickets []}))
 
@@ -32,14 +33,27 @@
    set-tickets-from-state
    set-ticket-from-state))
 
+(defn add-ticket-tab
+  "We do not have a good way to communicate from the generic View button
+  to find the active list item, so we can instead rely on the last state setting
+  for the current selected ticket in the list view."
+  [{:keys [ticket]}]
+  (let [ticket-id (:id ticket)]
+    (when ticket-id
+      (swap! *state update-in [:ticket-tabs] conj ticket-id))))
+
+(defn get-ticket-tabs [] (-> @*state :ticket-tabs))
+
 (defn event-handler [event]
   (case (:event/type event)
+    ::open-ticket (add-ticket-tab @*state)
     ::search (set-tickets-from-state @*state)
     ::set-ticket-id (swap-and-no-set [:ticket] event)
     ::set-ticket (set-ticket-from-state @*state)
-    ::stub (swap-and-no-set [:stub] event)))
+    ::stub (swap-and-no-set [:stub] event)
+    (prn "Unhandled event: " event)))
 
-(defn text-input [{:keys [label text]}]
+(defn text-input [{:keys [label text event-type]}]
   {:fx/type :v-box
    :spacing 5
    :padding 5
@@ -48,15 +62,15 @@
     {:fx/type :text-area
      :style {:-fx-font-family "monospace"}
      :text text
-     :on-text-changed {:event/type ::stub}}]})
+     :on-text-changed {:event/type event-type}}]})
 
 (defn button [{:keys [text event-type]}]
   {:fx/type :button
    :text text
    :on-action {:event/type event-type}})
 
-(defn ticket-button [& r]
-  (button {:text "Update" :event-type ::search}))
+(defn ticket-button [{:keys [text]} ]
+  (button {:text (str "View " text) :event-type ::open-ticket}))
 
 (defn search-button [& r]
   (button {:text "Search" :event-type ::search}))
@@ -70,7 +84,19 @@
      {:text (format "%s %s" id title)})
    :items tickets})
 
-(defn root [{:keys [stub ticket tickets]}]
+(defn render-ticket-tab [ticket-id]
+  {:fx/type :tab :text (str ticket-id)
+   :content {:fx/type :label :text "Coming soon"}})
+
+(defn render-ticket-tabs [main-children-map ticket-tabs]
+  (->>
+   (concat
+    [{:fx/type :tab :text "Main" :closable false
+      :content main-children-map}]
+    (map render-ticket-tab ticket-tabs))
+   (into [])))
+
+(defn root [{:keys [stub ticket ticket-tabs tickets]}]
   {:fx/type :stage
    :showing true
    :title "insectarium"
@@ -82,17 +108,17 @@
                   :children
                   [
                    {:fx/type :tab-pane
-                    :tabs [
-                           {:fx/type :tab :text "Hi" :closable false
-                            :content {:fx/type :label :text "Woopie"}}
-                           {:fx/type :tab :text "There" :closable false
-                            :content {:fx/type :label :text "Doopie"}}
-                           ]}
-                   {:fx/type text-input :label "Query params" :text stub}
-                   {:fx/type search-button}
-                   {:fx/type ticket-list :tickets tickets}
-                   {:fx/type text-input :label "Ticket Details" :text (str ticket)}
-                   {:fx/type ticket-button}
+                    :tabs
+                    (render-ticket-tabs
+                     {:fx/type :v-box
+                      :children
+                      [
+                       {:fx/type text-input :label "Query params" :text stub :event-type ::stub}
+                       {:fx/type search-button}
+                       {:fx/type ticket-list :tickets tickets}
+                       {:fx/type ticket-button}
+                       {:fx/type text-input :label "Ticket Preview" :text (:description ticket)}]}
+                     ticket-tabs)}
                    ]}}})
 
 (defn renderer []
